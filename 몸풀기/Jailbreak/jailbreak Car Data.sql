54fec1cb557d80a3ed60ad221ee843545cc8bf04
edaild <ch5183sb@gmail.com>
2025-01-08 19:16:26 +0900

    Fix : SQL쿼리문 수정

--- 몸풀기/Jailbreak/jailbreak Car Data.sql
+++ 몸풀기/Jailbreak/jailbreak Car Data.sql
@@ -1,4 +1,19 @@
 create database Car;
 use Car;
 
+# 일반 자동차 종류
+create table CarTable(num int auto_increment
+
+);
+
+# 스포츠카 종류
+
+
+# 특수차 종류
+
+
+# 비행기 종류
+
+
+# 해상 종류
 
--- 몸풀기/SQL 기초 쿼리문.sql
+++ 몸풀기/SQL 기초 쿼리문.sql
@@ -1,15 +1,15 @@
-# Database 생성
-create database user;
+   # Database 생성
+create database user_db;
 
 # Database 조회
-use user;
+use user_db;
 
 # database table 생성
 create table user(num int auto_increment,
  id varchar(32) not null,
  password varchar(32) not null,
  name varchar(32) not null,
- email varchar(12) not null,
+ email varchar(255) not null,
  primary key(num));
  
 # table 조회
@@ -18,16 +18,18 @@ select * from user;
 # table 조건 조회
 select * from user where num = 2;
 
+# num 기준으로 숫자가 가장 높은 수 부터 출력(select * from user order by num;와 select * from user order by num asc;는 서로 동일한 결과를 출력한다.) 
+select * from user order by num desc;
 
 # database 레코드 추가
-insert into user(id, password, name, email) value("User01","user1234","User01","user01@g");
-insert into user(id, password, name, email) value("User02","user2222","User02","user02@g");
-insert into user(id, password, name, email) value("User03","user3333","User03","user03@g");
-insert into user(id, password, name, email) value("User04","user4444","User04","user04@g");
-insert into user(id, password, name, email) value("User05","user5555","User05","user05@g");
+insert into user(id, password, name, email) value("User01","user1234","User01","user01@gmail.com");
+insert into user(id, password, name, email) value("User02","user2222","User02","user02@gmail.com");
+insert into user(id, password, name, email) value("User03","user3333","User03","user03@gmail.com");
+insert into user(id, password, name, email) value("User04","user4444","User04","user04@gmail.com");
+insert into user(id, password, name, email) value("User05","user5555","User05","user05@gmail.com");
 
 # database 레코드 수정 (update문)
-update user set password = "user1234", password = "user1111" where num = 1;
+update user set password = "user1111" where num = 1;
 
 # database 레코드 삭제
 delete from user where num = 5;
@@ -36,5 +38,5 @@ delete from user where num = 5;
 drop table user;
 
 # databass 삭제
-drop database user;
+drop database user_db;
 
