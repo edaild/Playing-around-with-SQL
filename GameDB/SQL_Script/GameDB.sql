9b09496f890616137498bb31c0dd07d0a3c83505
edaild <ch5183sb@gmail.com>
2025-02-14 17:35:38 +0900

    Feat : Day02 ERD 재설계 후 DB 구현

--- GameDB/SQL_Script/GameDB.sql
+++ GameDB/SQL_Script/GameDB.sql
@@ -1,29 +1,65 @@
 create database Gamedb_test;
 use gamedb_test;
 
-# user table
-create table Usertable(
-usernumber int not null,
-id varchar(32) not null,
-password varchar(32) not null,
-name varchar(32) not null,
-eamil varchar(32) not null,
-chche int not null,
-primary key(usernumber)
+# User
+create table user(
+user_uid int not null,
+user_id varchar(32) not null,
+user_pw varchar(32) not null,
+user_name varchar(32) not null,
+user_email varchar(255) not null,
+primary key(user_uid)
 );
 
-# Inventorytalbe
-create table Inventorytable(
-itemnumber int not null,
-itemname varchar(32) not null,
-itemskillcode varchar(32) not null,
-quantity int not null,
-usernumber int not null,
-primary key(itemnumber)
+# user_Gamedata
+create table user_gamedata(
+game_id int not null,
+user_uid int not null,
+user_character_name varchar(25) not null,
+user_lever int not null,
+user_gamemoney int not null,
+user_gametime time not null,
+primary key(game_id)
 );
 
-alter table Inventorytable add constraint foreign key(usernumber) references Usertable (usernumber);
+# Item
+create table item(
+item_id int not null,
+item_name varchar(32) not null,
+item_type varchar(32) not null,
+item_skill varchar(32) not null,
+item_lever varchar(32) not null,
+item_price int not null,
+primary key(item_id)
+)
 
+# Inventory
+create table inventory(
+inventory_id int auto_increment,
+item_id int not null,
+item_inventory_quantity int not null,
+primary key(inventory_id)
+);
 
+# Store
+create table store(
+store_id int auto_increment,
+item_id int not null,
+item_store_quantity int not null,
+primary key(store_id)
+)
 
+# user_game_data_inventory_store
+create table user_game_data_inventory_store(
+game_id int not null,
+inventory_id int not null,
+store_id int not null
+)
 
+# foreign key 
+alter table user_gamedata add constraint foreign key (user_uid) references user (user_uid);
+alter table inventory add constraint foreign key (item_id) references item (item_id);
+alter table store add constraint foreign key (item_id) references item (item_id);
+alter table user_game_data_inventory_store add constraint foreign key (game_id) references user_gamedata(game_id);
+alter table user_game_data_inventory_store add constraint foreign key (inventory_id) references inventory(inventory_id);
+alter table user_game_data_inventory_store add constraint foreign key (store_id) references store(store_id);
